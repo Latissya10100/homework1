483d797d633e02f4f6de068bf59c6a4e94571d83
Latissya Speller <latissyaspeller@Latissyas-MBP.home>
2016-10-16 21:25:35 -0400

    added Assignment_17

---
+++ assignment_17/js/app.js
@@ -0,0 +1,8 @@
+var obj = {"video": {
+  "value": "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='http://www.youtube.com/embed/W-Q7RMpINVo' frameborder='0' allowFullScreen></iframe>"
+}}
+document.write(obj.video.value);
+var str="My Name is Latissya";
+document.write(str.toUpperCase());
+document.write("<br>");
+document.write("99"+" "+"problems but a 'script' ain't one");
