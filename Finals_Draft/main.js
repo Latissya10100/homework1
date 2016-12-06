1540637b048831b4ff9bcb8dac92d6c8ddd52f75
Latissya Speller <latissyaspeller@Latissyas-MacBook-Pro.local>
2016-12-06 13:35:32 -0500

    finals draft folder added

---
+++ Finals_Draft/main.js
@@ -0,0 +1,43 @@
+$(function (){
+
+	var $orders = $('#orders');
+	var $name = $('#name');
+	var $drink = $('#drink');
+})
+function addOrder(order) {
+	$orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
+
+}
+$.ajax({
+	type: 'GET',
+	url: '/rest.learncode.academy/api/johnbob/friends',
+	success: function(orders) {
+		$.each(orders, function(i, order) {
+			addOrder(order);	
+		});
+	},
+	error: function() {
+		alert('error loading orders');
+	}
+});
+$('#add-order').on('click', function() {
+
+	var order = {
+		name: $name.val(),
+		drink: $drink.val(),
+	};
+
+	$.ajax({
+		type: 'POST',
+		url: '/rest.learncode.academy/api/johnbob/friends',
+		data: order,
+		success: function(newOrder) {
+			addOrder(newOrder);	
+		},
+		error: function() {
+			alert('error saving order');
+		}
+	});
+
+});
+
