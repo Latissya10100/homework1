0997e6203617465f016259e5b00698e0e49ee5ee
Latissya Speller <latissyaspeller@Latissyas-MacBook-Pro.local>
2016-12-09 17:02:56 -0500

    additional changes to Final_Draft

--- Finals_Draft/main.js
+++ Finals_Draft/main.js
@@ -1,43 +1,48 @@
 $(function (){
 
-	var $orders = $('#orders');
-	var $name = $('#name');
-	var $drink = $('#drink');
-})
-function addOrder(order) {
-	$orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
+	var orders = $('#orders');
+	var name = $('#name');
+	var drink = $('#drink');
 
-}
-$.ajax({
-	type: 'GET',
-	url: '/rest.learncode.academy/api/johnbob/friends',
-	success: function(orders) {
-		$.each(orders, function(i, order) {
-			addOrder(order);	
-		});
-	},
-	error: function() {
-		alert('error loading orders');
+
+	function addOrder(order) {
+		orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
+
 	}
-});
-$('#add-order').on('click', function() {
-
-	var order = {
-		name: $name.val(),
-		drink: $drink.val(),
-	};
-
 	$.ajax({
-		type: 'POST',
-		url: '/rest.learncode.academy/api/johnbob/friends',
-		data: order,
-		success: function(newOrder) {
-			addOrder(newOrder);	
+		type: 'GET',
+		url: 'http://rest.learncode.academy/api/johnbob/friends',
+		success: function(orders) {
+			$.each(orders, function(i, order) {
+				addOrder(order);	
+			});
 		},
 		error: function() {
-			alert('error saving order');
+			alert('error loading orders');
 		}
 	});
+	$('#add-order').on('click', function() {
 
-});
+		console.log('asdf');
+
+		var order = {
+			name: name.val(),
+			drink: drink.val(),
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: 'http://rest.learncode.academy/api/johnbob/friends',
+			data: order,
+			success: function(newOrder) {
+				addOrder(newOrder);	
+			},
+			error: function() {
+				alert('error saving order');
+			}
+		});
+
+	});
+
+})
 
