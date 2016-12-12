fd99219b38f6f6455e0f4ea8ce97966ba8c3bf76
Latissya Speller <latissyaspeller@Latissyas-MacBook-Pro.local>
2016-12-12 17:50:43 -0500

    final completed version for grading

--- Finals_Draft/main.js
+++ Finals_Draft/main.js
@@ -3,56 +3,19 @@ $(function (){
 	var orders = $('#orders');
 	var name = $('#name');
 	var drink = $('#drink');
-<<<<<<< HEAD
 
-=======
-
-
-function addOrder(order) {
-	orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
-
-}
-$.ajax({
-	type: 'GET',
-	url: 'http://rest.learncode.academy/api/johnbob/friends',
-	success: function(orders) {
-		$.each(orders, function(i, order) {
-			addOrder(order);	
-		});
-	},
-	error: function() {
-		alert('error loading orders');
-	}
-});
-$('#add-order').on('click', function() {
-
-	console.log('asdf');
-
-	var order = {
-		name: name.val(),
-		drink: drink.val(),
-	};
->>>>>>> a3f126db6e2117053c6bdd3a67aa788e8a812be9
 
 	function addOrder(order) {
 		orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
 
 	}
 	$.ajax({
-<<<<<<< HEAD
 		type: 'GET',
 		url: 'http://rest.learncode.academy/api/johnbob/friends',
 		success: function(orders) {
 			$.each(orders, function(i, order) {
 				addOrder(order);	
 			});
-=======
-		type: 'POST',
-		url: 'http://rest.learncode.academy/api/johnbob/friends',
-		data: order,
-		success: function(newOrder) {
-			addOrder(newOrder);	
->>>>>>> a3f126db6e2117053c6bdd3a67aa788e8a812be9
 		},
 		error: function() {
 			alert('error loading orders');
@@ -80,7 +43,5 @@ $('#add-order').on('click', function() {
 		});
 
 	});
-
-})
 
 })
